5e28e7f5473fa2f405a1bc869fecf9adee6a5267
Pavel Timofeev <ttt.1.12@bk.ru>
2022-05-22 16:49:31 +0700

    Initial Commit

---
+++ Project 2 Flag/Project_2_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2_FlagApp.swift
+//  Project 2 Flag
+//
+//  Created by Павел Тимофеев on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_2_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

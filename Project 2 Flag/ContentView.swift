90eb18889e5ddf3f2405948430581381012b9b48
Pavel Timofeev <ttt.1.12@bk.ru>
2022-05-23 13:12:34 +0700

    challenge for project 2 had done

--- Project 2 Flag/ContentView.swift
+++ Project 2 Flag/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var showing = false
     @State private var scoreTitle = ""
-    @State private    var score = 0
+    @State private var score = 0
+    @State private var question = 0
     
     @State private   var countries = [ "Estonia", "France", "Germany",
-    "Ireland", "Italy", "Nigeria", "Poland", "Russia","Spain", "UK", "US"                      ]
+    "Ireland", "Italy", "Nigeria", "Poland", "Russia","Spain", "UK", "US" ]
     @State private   var correctAnswer = Int.random(in: 0...2)
     
     
@@ -27,38 +28,56 @@ struct ContentView: View {
  
         }
         ForEach(0..<3) {    number in
-            Button {
-            flagTapped(number)
-            } label: {
+            Button {    flagTapped(number)
+                            } label: {
                 Image(countries[number]).renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
             }
         }
             Text("SCORE- \(score)").foregroundColor(.white).font(.largeTitle)
             
     }
-    }
-        .alert(scoreTitle, isPresented: $showing) {
-            Button("Continue", action: askQuestion)
+    } // chalenge 3
+        .alert(scoreTitle, isPresented: $showing) { if question == 8 {
+            Button("Reset", action: reset)
+        } else {
+            Button("Continue", action: askQuestion) }
         } message: {
+            Text("Your score is \(score)")
+
+
             // chalenge 1
-            Text("Your score is ")
+            Text("Your score is \(score)")
         }
 }
     func flagTapped(_ number: Int) {
+        
+     
         if number == correctAnswer {
             scoreTitle = "Correct"
             score += 1
+            question += 1
     } else {
-        scoreTitle = "Wrong"
+        // chalenge 2
+        scoreTitle = "Wrong!!!" + " " +
+        "That's the flag of \(countries[number])"
+        question += 1
+
     }
     showing = true
     
     
 }
+    
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    func reset () {
+        question = 0
+        score = 0
+        askQuestion()
+    }
+    
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
